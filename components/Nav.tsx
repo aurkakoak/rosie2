b62b7edbe2fdc8e3d3bca04c50436966ebbfdea7
Tom Jennings <tom.jennings@beamery.com>
2023-03-03 22:24:27 +0000

    fix nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,21 @@
-import { html } from "https://deno.land/x/html/mod.ts";
-
+const items = [
+  { name: "About", path: "/about" },
+  { name: "Case Study", path: "/case-study" },
+  { name: "Blog", path: "/blog" },
+  { name: "Contact", path: "/contact" },
+];
 export function Nav() {
-  return <nav class="bg-gray-800">
+  return (
+    <nav class="bg-gray-800">
       <ul class="flex justify-between items-center px-8 py-4">
-        <li><a href="/about" class="text-gray-300 hover:text-white">About</a></li>
-        <li><a href="/case-study" class="text-gray-300 hover:text-white">Case Study</a></li>
-        <li><a href="/blog" class="text-gray-300 hover:text-white">Blog</a></li>
-        <li><a href="/contact" class="text-gray-300 hover:text-white">Contact</a></li>
+        {items.map((item) => (
+          <li>
+            <a href={item.path} class="text-gray-300 hover:text-white">
+              {item.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
-  ;
+  );
 }
